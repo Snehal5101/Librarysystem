ea94854002a3070f2e1a3f6ee3ca2cf8ce205830
Snehal5101 <manesnehal357@gmail.com>
2021-11-01 10:30:48 +0530

    initial

---
+++ app.js
@@ -0,0 +1,3 @@
+let card=[{img:url("https://th.bing.com/th/id/OIP.f3ZelF_WuKK-F1ISMdxAXAHaHa?pid=ImgDet&rs=1"),img:url("https://cdn.penguin.com.au/covers/original/9780141332499.jpg"),img:url("https://i.harperapps.com/covers/9780062024329/y648.jpg"),img:url("http://ecx.images-amazon.com/images/I/514GAjvGPTL._SY445_QL70_.jpg")}];
+
+document.querySelector(".card").innerHTML=card;
